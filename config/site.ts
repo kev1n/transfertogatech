648cc4d54ec763e4d1531e0c07d1587ee4d7e26c
Kevin Wang <kwang30@stuy.edu>
2023-09-04 14:23:28 -0400

    www link

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "transfer to gatech",
   description: "A tool to help prospective students transfer to Georgia Tech.",
-  url: "https://transfertogatech.com",
+  url: "https://www.transfertogatech.com",
   links: {
     github: "https://github.com/kev1n/transfertogatech",
   },
