d894492e1abc9da2a62c332ecb6daf730e7507f6
Kevin Wang <kwang30@stuy.edu>
2023-09-04 14:03:28 -0400

    prod

--- lib/utils/db-consumer/getSchools.ts
+++ lib/utils/db-consumer/getSchools.ts
@@ -18,7 +18,7 @@ export const checkEnvironment = () => {
   let base_url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://transfertogatech.vercel.app";
+      : "https://transfertogatech.com";
 
   return base_url;
 };
