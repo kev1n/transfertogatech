df8db5375d451506cd6ba4bc1117fb2ef1adf7c2
Kevin Wang <kwang30@stuy.edu>
2023-09-04 18:07:09 -0400

    speed

--- app/api/mongo/monthlyRefreshData/route.ts
+++ app/api/mongo/monthlyRefreshData/route.ts
@@ -4,6 +4,10 @@ import mongoImportSchools from "@/lib/utils/mongo-helper/mongoImportSchools";
 import mongoImportStates from "@/lib/utils/mongo-helper/mongoImportStates";
 import mongoImportEquivalency from "@/lib/utils/mongo-helper/mongoImportEquivalency";
 import mongoMonthlyRequestLimiter from "@/lib/utils/mongo-helper/mongoMonthlyRequestLimiter";
+import { MongoClient } from "mongodb";
+import { Class, School } from "@/types/mongo/mongotypes";
+import getAllSubjectsInSchool from "@/lib/utils/api-helper/getAllSubjectsInSchool";
+import getEquivalencyForSchool from "@/lib/utils/api-helper/getEquivalencyForSchool";
 
 export const dynamic = "force-dynamic";
 
@@ -25,15 +29,69 @@ export async function GET() {
   const allSchools = await mongoImportSchools(client, states);
   console.log(`Gathering equivalencies for ${allSchools.length} schools`);
 
-  const batchSize = 200; // Adjust this based on what works for your system
-  for (let i = 0; i < allSchools.length; i += batchSize) {
-    const batch = allSchools.slice(i, i + batchSize);
-    await Promise.all(
-      batch.map((school) => mongoImportEquivalency(client, school))
-    );
-  }
+  const equivalenciesForAllSchools = await gatherEquivalencies(allSchools);
+  bulkImportEquivalencies(client, equivalenciesForAllSchools);
+  /*
+  for (let i = 0; i < allSchools.length; i++) {
+    mongoImportEquivalency(client, allSchools[i]);
+  }*/
 
   return new Response(JSON.stringify({ success: true }));
+}
+
+export async function gatherEquivalencies(schools: School[]) {
+  const operationsPromises = schools.map((school) =>
+    gatherEquivalencyForSchool(school)
+  );
+
+  const equivalenciesForAllSchools = await Promise.all(operationsPromises);
+  return equivalenciesForAllSchools.filter(Boolean); // this removes any null values
+}
+
+async function gatherEquivalencyForSchool(school: School) {
+  try {
+    const schoolData = await getAllSubjectsInSchool(school.state, school.id);
+    const term = schoolData.terms[0].id;
+
+    const equivalents = await getEquivalencyForSchool(
+      school.state,
+      school.id,
+      schoolData.subjects,
+      term
+    );
+
+    console.log(
+      `Gathered ${equivalents.length} equivalencies for ${school.name}`
+    );
+
+    return {
+      updateOne: {
+        filter: { _id: school.id },
+        update: {
+          $set: {
+            _id: school.id,
+            school: school.name,
+            equivalents: equivalents,
+            term: term,
+          },
+        },
+        upsert: true,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    // Handle the error, e.g., retry mechanism or logging
+    return null; // or a default value, or you could re-throw the error depending on your use case
+  }
+}
+
+export async function bulkImportEquivalencies(
+  client: MongoClient,
+  operations: any[]
+) {
+  const db = client.db("transfer");
+  const collection = db.collection("equivalents");
+  return collection.bulkWrite(operations);
 }
 
 const states = [
